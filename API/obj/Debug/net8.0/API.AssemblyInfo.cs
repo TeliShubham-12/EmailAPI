087a25d6e5846dc4504b340f771f0b424bec3b38
TeliShubham-12 <shubhamteli203@gmail.com>
2025-01-02 22:21:34 +0530

    .

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f3cdeb6d32ae9b9f3b802d2232c87d5706595b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e257591db2a73bede3c559f7b1fe02179feb880")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
